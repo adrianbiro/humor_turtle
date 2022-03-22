d13600b4566c64bf54589c7b3ce43da11c514862
Adrián Bíro <biroadrian@protonmail.com>
2022-03-22 20:23:47 +0100

    prva vec

---
+++ buchta_turtle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import turtle
+
+ob = turtle.Turtle()
+ob.speed(10)
+ob.color("black", "blue")
+ob.goto(0, 0)
+
+
+
+while True:
+    ob.forward(200)
+    ob.left(170)
+    for i in range(100):
+        if (i % 2) == 0:
+            ob.forward(100)
+            ob.right(90)
+        else:
+            ob.forward(i / 2)
+            ob.right(i * 2)
+
+
+
+turtle.done()
